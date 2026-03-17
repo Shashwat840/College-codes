887de0b6c75598184afb13defe10a8b48b6da31e
Dave Shashwat <daveshashwat46@gmail.com>
2026-03-17 17:46:24 +0530

    strstr.c , strlen.c

--- String/Strstr.c
+++ String/Strstr.c
@@ -12,4 +12,5 @@ int main(){
     else{
         printf("\nString found...");
     }
+    return 0;
 }
---
+++ String/strlen.c
@@ -0,0 +1,14 @@
+//Find length of string without strlen
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[20];
+    int len=0;
+    printf("\nEnter string:- ");
+    gets(str);
+    for(int i =0;i!= '\0';i++){
+        len++;
+    }
+    printf("\nLength of string is:- %d",len);
+    return 0;
+}
